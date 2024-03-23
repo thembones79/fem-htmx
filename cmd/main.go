7ea05e601b503b22bd67711b6b53245a4d94b34c
thembones79 <michal.szumnarski@gmail.con>
2024-03-23 09:50:02 +0100

    form

--- cmd/main.go
+++ cmd/main.go
@@ -57,16 +57,19 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-    data := newData()
+	data := newData()
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", data)
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "display", count)
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		data.Contacts = append(data.Contacts, newContact(name, email))
+		return c.Render(200, "display", data)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
